c1e6cfc4c742f40abc4901d9d6f9f78399fd8dcf
Olena Ageyeva <eageeva@hotmail.com>
2018-05-16 12:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function driversWithRevenueOver(drivers, revenue){
   return drivers.filter(function(driver){return driver.revenue>revenue})
 }
 
-function driverNamesWithRevenueOver(){
-  return driversWithRevenueOver.map(function(driver){return driver.name})
+function driverNamesWithRevenueOver(drivers){
+  return drivers.filter(function(driver){return driver.revenue>revenue}).map(function(driver){return driver.name})
 }
 
 function exactMatch(){
